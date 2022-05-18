b4e940daabbd85555fb784285cde403e8c6c491a
Sergio <webdev.slara@gmail.com>
2022-05-18 19:56:49 +0200

    worked with input template and its class, added several ngIfs and ngClass, learned about assertion in TS

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -8,11 +8,19 @@ import { FormControl } from '@angular/forms'
 })
 export class InputComponent implements OnInit {
 
+  //* assertion ! operator asserts that the value is not null or undefined
   @Input() control!: FormControl;
+  @Input() label!: string
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showErrors(){
+    const { dirty, touched, errors } = this.control
+    return dirty && touched && errors
+    //return this.control.dirty && this.control.touched && this.control.errors
   }
 
 }
